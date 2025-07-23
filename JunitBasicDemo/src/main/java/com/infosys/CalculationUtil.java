c035628fe2b82296a02b6d09f304e6334d5aebb6
vaibhavj27 <vj9766876963@gmail.com>
2025-07-23 20:02:16 +0530

    Session date-23/7/2025

---
+++ JunitBasicDemo/src/main/java/com/infosys/CalculationUtil.java
@@ -0,0 +1,20 @@
+package com.infosys;
+
+public class CalculationUtil {
+    public int add(int a,int b){
+        return a+b;
+    }
+
+    public int multiply(int a , int b)
+    {
+        return a*b;
+    }
+
+    public String parseString(String input){
+        return input;
+    }
+
+    public int Division(int a,int b){
+        return a/b;
+    }
+}
--- JunitBasicDemo/src/test/java/com/infosys/AppTest.java
+++ JunitBasicDemo/src/test/java/com/infosys/AppTest.java
@@ -1,38 +1,73 @@
 package com.infosys;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    //all test cases run parallely
+    @Test
+    void testshow()
     {
-        super( testName );
+        System.out.println("Hello world");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+    @Test
+    @DisplayName("adding test")
+    void AddTest(){
+        CalculationUtil cu=new CalculationUtil();
+        int actual=cu.add(20,10);
+        int expected=30;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    @DisplayName("multiply test")
+    void MultiplyTest(){
+        CalculationUtil cu=new CalculationUtil();
+        int actual=cu.multiply(10,20);
+        int expected=200;
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    @Disabled //we can disable any particular test with this annotation
+    void FailedTest()
     {
-        return new TestSuite( AppTest.class );
+        Assertions.fail("This test is going to fail always");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    void StringNotNullTest()
     {
-        assertTrue( true );
+        CalculationUtil cu=new CalculationUtil();
+        String input="abcd";
+       String res =cu.parseString(input);
+       try{
+           Assertions.assertNotNull(res);
+       } catch (Exception e) {
+           throw new NullPointerException();
+       }
+    }
+
+    @Test
+    void DivTest(){
+        CalculationUtil cu= new CalculationUtil();
+        Assertions.assertThrows(ArithmeticException.class,()->cu.Division(10,0));
+        Assertions.assertDoesNotThrow(()->cu.Division(10,2),"This will generate arithmatic exception in code");
+    }
+
+    @Test
+    void CompareObject()
+    {
+        // these two will be stored in String pool i.e both will refer to same object as "hi"="hi"
+        String s1="hi";
+        String s2="hi";
+        Assertions.assertSame(s1,s2);
+        //this notation generate new object and store in Heap memory
+        String S3=new String("hi");
+        String S4=new String("hi");
+        //even for string  S3 and S4 will test fail as they point to different objects
+       // Assertions.assertSame(s1,S3);
     }
 }
